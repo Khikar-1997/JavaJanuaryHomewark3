378527501368af25d5f3eddf564014814681c798
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 05:13:13 -0800

    3_6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,26 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> colors = new ArrayList<>();
+        LinkedList<String> colors = new LinkedList<>();
         colors.add("Red");
         colors.add("Green");
         colors.add("Yellow");
         colors.add("Black");
         colors.add("White");
 
-        System.out.println(colors.get(2));
+        for (String s : colors) {
+            System.out.println(s);
+        }
+        System.out.println();
+
+        colors.remove(2);
+
+        for (String color : colors) {
+            System.out.println(color);
+        }
     }
 }
