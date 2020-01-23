05479f51fbae5d0e22159653020b54ae97d868b8
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 07:22:10 -0800

    3-12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,43 +14,14 @@ public class Main {
         months.add("Jule");
         months.add("August");
         months.add("September");
-        System.out.println("This is 'months' array");
+        System.out.println("This is 'months' array before elemenths swaping");
         for (String month : months) {
             System.out.print(month + " ,");
         }
-
         System.out.println();
-        ArrayList<String> weekDays = new ArrayList<>();
-        weekDays.add("Monday");
-        weekDays.add("Tuesday");
-        weekDays.add("Wednesday");
-        weekDays.add("Thursday");
-        weekDays.add("Friday");
-        weekDays.add("Saturday");
-        weekDays.add("Sunday");
-        System.out.println("This is 'weekDays' array");
-        for (String weekDay : weekDays) {
-            System.out.print(weekDay + " ,");
-        }
-        System.out.println();
-
-
-        ArrayList<String> months1 = new ArrayList<>();
-        months1.add("March");
-        months1.add("April");
-        months1.add("January");
-        months1.add("June");
-        months1.add("Jule");
-        months1.add("August");
-        months1.add("September");
-        System.out.println("This is 'months1' array");
-        for (String month : months1) {
-            System.out.print(month + " ,");
-        }
-
-        System.out.println();
-        System.out.println("'months' equals 'weekDays' = " + months.equals(weekDays));
-        System.out.println("'months' equals 'months1' = " + months.equals(months1));
+        Collections.swap(months,0,2);
+        System.out.println("This is 'months' array after elemenths swaping");
+        System.out.println(months);
     }
 }
 
