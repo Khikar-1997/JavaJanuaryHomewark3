db4aa1d92b2a0c9dcbcd61bdb31efb440338c8ea
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 03:00:05 -0800

    2_2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> colors = new ArrayList<>();
-        colors.add("Red");
-        colors.add("Green");
-        colors.add("Yellow");
-        colors.add("Black");
-        colors.add("Orange");
+        colors.add(" Red");
+        colors.add(" Green");
+        colors.add(" Yellow");
+        colors.add(" Black");
+        colors.add(" Orange");
         for (int i = 0; i < colors.size(); i++) {
-            System.out.println(colors.get(i));
+            System.out.println(colors.iterator());
         }
     }
 }
