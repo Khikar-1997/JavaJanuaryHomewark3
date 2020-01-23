ea39294cc7fa8cd6de0ee240f8dbb7d295e02fbe
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 05:05:59 -0800

    3_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,19 +12,7 @@ public class Main {
         colors.add("Yellow");
         colors.add("Black");
         colors.add("White");
-        System.out.println("The 'colors' array before adding element");
 
-        for (String color : colors) {
-            System.out.print(color + " ,");
-        }
-
-        System.out.println();
-        System.out.println();
-        System.out.println("The 'colors' array after adding element");
-
-        colors.add(0,"Orange");
-        for (String color : colors) {
-            System.out.print(color + " ,");
-        }
+        System.out.println(colors.get(2));
     }
 }
