705170af63e5f5fafeab0073b63149416fa76c5a
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 02:14:32 -0800

    3_1

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Black");
+        colors.add("Orange");
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));
+        }
+    }
+}
