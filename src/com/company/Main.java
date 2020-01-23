088a9d68e42975d95588cf5e7f489f3731ad6f5d
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 03:26:11 -0800

    3_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,24 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> colors = new ArrayList<>();
-        colors.add(" Red");
-        colors.add(" Green");
-        colors.add(" Yellow");
-        colors.add(" Black");
-        colors.add(" White");
-        Iterator<String> colorsIterator = colors.iterator();
-        for (int i = 0; colorsIterator.hasNext(); i++) {
-            String color = colorsIterator.next();
-            System.out.println(color);
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Black");
+        colors.add("White");
+        System.out.println("The 'colors' array before adding element");
+
+        for (String color : colors) {
+            System.out.print(color + " ,");
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println("The 'colors' array after adding element");
+
+        colors.add(0,"Orange");
+        for (String color : colors) {
+            System.out.print(color + " ,");
         }
     }
 }
