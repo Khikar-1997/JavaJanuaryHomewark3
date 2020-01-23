03d4503e6720a3988057c2fd2429b3e4305c72df
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 06:41:38 -0800

    3_5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,28 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-
-        numbers.add(34);
-        numbers.add(28);
-        numbers.add(1);
-        numbers.add(78);
-        numbers.add(100);
-
-        System.out.println("This 'numbers' array");
-        for (int number : numbers) {
-            System.out.print(number + " ,");
+        ArrayList<String> months = new ArrayList<>();
+        months.add("March");
+        months.add("April");
+        months.add("January");
+        months.add("June");
+        months.add("Jule");
+        months.add("August");
+        System.out.println("This is 'months' array before update");
+        for (String month : months) {
+            System.out.print(month + " ,");
         }
-
         System.out.println();
-        ArrayList<Integer> copyNumbers = new ArrayList<>(numbers);
-        System.out.println("And this is'copyNumbers' array,it is copy of 'numbers' array");
-        for (Integer copyNumber : copyNumbers) {
-            System.out.print(copyNumber + " ,");
+        months.remove(2);
+        months.add("May");
+        System.out.println("This is 'months' array after update.");
+        for (String month : months) {
+            System.out.print(month + " ,");
         }
     }
 }
+
