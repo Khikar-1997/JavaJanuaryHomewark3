caa7ee86e7483e0f18a07fcf435b6447e3c72f51
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 08:35:07 -0800

    3-17

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,27 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> weekDays = new ArrayList<>();
         weekDays.add("Monday");
-        weekDays.add("Tuesday");
         weekDays.add("Wednesday");
         weekDays.add("Thursday");
         weekDays.add("Friday");
         weekDays.add("Saturday");
         weekDays.add("Sunday");
-        System.out.println("This is 'weekDays' array.");
+        System.out.println("This is 'weekDays' array before replace.");
         for (String weekDay : weekDays) {
             System.out.print(weekDay + " ,");
         }
         System.out.println();
 
-        System.out.println("This is 'months' array after clear,it is empty");
-        weekDays.clear();
+        weekDays.set(1, "Tuesday");
+        System.out.println("This is 'months' array after replace.");
         System.out.println(weekDays);
-        if (weekDays.size() == 0) {
-            System.out.println("It is empty");
-        } else {
-            System.out.println("'weekDays' arrays length is " + weekDays.size());
-        }
     }
 }
 
