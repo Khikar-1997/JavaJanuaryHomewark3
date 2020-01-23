3690e25487d15ce3e9c54cecbdf5bf4a48fb7f2a
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 07:15:37 -0800

    3-11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,15 +13,44 @@ public class Main {
         months.add("June");
         months.add("Jule");
         months.add("August");
-        System.out.println("This is 'months' array before reverse");
+        months.add("September");
+        System.out.println("This is 'months' array");
         for (String month : months) {
             System.out.print(month + " ,");
         }
-        System.out.println();
-        System.out.println("This is 'months' array after reverse.");
-        Collections.reverse(months);
-        System.out.println(months);
 
+        System.out.println();
+        ArrayList<String> weekDays = new ArrayList<>();
+        weekDays.add("Monday");
+        weekDays.add("Tuesday");
+        weekDays.add("Wednesday");
+        weekDays.add("Thursday");
+        weekDays.add("Friday");
+        weekDays.add("Saturday");
+        weekDays.add("Sunday");
+        System.out.println("This is 'weekDays' array");
+        for (String weekDay : weekDays) {
+            System.out.print(weekDay + " ,");
+        }
+        System.out.println();
+
+
+        ArrayList<String> months1 = new ArrayList<>();
+        months1.add("March");
+        months1.add("April");
+        months1.add("January");
+        months1.add("June");
+        months1.add("Jule");
+        months1.add("August");
+        months1.add("September");
+        System.out.println("This is 'months1' array");
+        for (String month : months1) {
+            System.out.print(month + " ,");
+        }
+
+        System.out.println();
+        System.out.println("'months' equals 'weekDays' = " + months.equals(weekDays));
+        System.out.println("'months' equals 'months1' = " + months.equals(months1));
     }
 }
 
